400b549957afbacf8d1d1504e4c0e1df320abe6f
Christophe Brun <christophe.brun.cl194@gadz.org>
2014-01-17 12:39:11 +0100

    replace subprocess call adb byu pyadb librarie

--- adbwireless_server.py
+++ adbwireless_server.py
@@ -1,23 +1,21 @@
 from twisted.web import server, resource
 from twisted.internet import reactor
 from string import split
-import subprocess
+from pyadb import ADB
 
-class HelloResource(resource.Resource):
+class AdbWirelessServer(resource.Resource):
     isLeaf = True
-    numberRequests = 0
                 
     def render_GET(self, request):
-        self.numberRequests += 1
-        request.setHeader("content-type", "text/plain")
+        adb = ADB('/usr/bin/adb')
+        retour=''
         items=split(request.path,'/')[1:]
         if items[0]=='c':
-            retour=subprocess.call('adb connect %s' % items[1],shell=True)
-            print "connexion avec %s" % items[1]
+            retour=adb.connect_remote(items[1])
         elif items[0]=='d':
-            retour=subprocess.call('adb disconnect %s' % items[1],shell=True)
-            print "Deconnexion avec %s" % items[1]
+            retour=adb.disconnect_remote(items[1])
+        print "adb : %s" % retour 
         return "OK\n"
 
-reactor.listenTCP(8555, server.Site(HelloResource()))
+reactor.listenTCP(8555, server.Site(AdbWirelessServer()))
 reactor.run()
